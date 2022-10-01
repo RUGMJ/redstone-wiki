9a321ba8924876803acda66b1b3b98715b04b1cd
RUGMJ7443 <matthewrhysjones@icloud.com>
2022-10-01 19:32:34 +0100

    Added cloudflare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
+import remarkEmoji from 'remark-emoji';
+import cloudflare from "@astrojs/cloudflare";
 const target = ['chrome87', 'edge88', 'es2022', 'firefox78', 'safari14'];
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx({
+    remarkPlugins: [remarkEmoji]
+  })],
   vite: {
     build: {
       target
@@ -15,5 +19,7 @@ export default defineConfig({
       target
     }
   },
-  site: 'https://redstone.rugmj.dev'
+  site: 'https://redstone.rugmj.dev',
+  output: "server",
+  adapter: cloudflare()
 });
