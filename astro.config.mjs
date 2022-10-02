2559fdff334ffe5420a9c8e1a8a75062fed1c84e
RUGMJ7443 <matthewrhysjones@icloud.com>
2022-10-02 11:02:16 +0100

    temp disabled icons  because cloudflare doesnt support the package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,26 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import remarkEmoji from 'remark-emoji';
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 const target = ['chrome87', 'edge88', 'es2022', 'firefox78', 'safari14'];
-
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx({
-    remarkPlugins: [remarkEmoji]
-  })],
-  vite: {
-    build: {
-      target
-    },
-    esbuild: {
-      target
-    }
-  },
-  site: 'https://redstone.rugmj.dev',
-  output: "server",
-  adapter: cloudflare()
+	integrations: [
+		react(),
+		mdx({
+			remarkPlugins: [remarkEmoji],
+		}),
+	],
+	vite: {
+		build: {
+			target,
+		},
+		esbuild: {
+			target,
+		},
+	},
+	site: 'https://redstone.rugmj.dev',
+	output: 'server',
+	adapter: cloudflare(),
 });
